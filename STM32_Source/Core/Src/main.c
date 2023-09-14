0af6a325c353e52ccff8eb873f2496d37602c239
QuantumMarshal <quangnhan1411@gmail.com>
2023-09-14 19:07:46 +0700

    Update main.c and STM32_LAB1.pdsprj

--- STM32_Source/Core/Src/main.c
+++ STM32_Source/Core/Src/main.c
@@ -96,20 +96,20 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
 	  if (counter == 2){
-		  HAL_GPIO_WritePin ( LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET ) ;
-		  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET ) ;
+		  HAL_GPIO_WritePin ( LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET ) ;
+		  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET ) ;
 	  }
 
 	  else if (counter == 4){
-		  HAL_GPIO_WritePin ( LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET ) ;
-		  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET ) ;
+		  HAL_GPIO_WritePin ( LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET ) ;
+		  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET ) ;
 		  counter = -1;
 	  }
 	  counter++;
 
 	  HAL_Delay(1000);
+    /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
