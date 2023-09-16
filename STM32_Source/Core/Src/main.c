90120688fd245ffe96ac1fcb365abbd7a66849f4
QuantumMarshal <quangnhan1411@gmail.com>
2023-09-16 13:34:14 +0700

    Update main.c Ex9

--- STM32_Source/Core/Src/main.c
+++ STM32_Source/Core/Src/main.c
@@ -260,28 +260,28 @@ void clearNumberOnClock(int num){
 		HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
 		break;
 	case 4:
-		HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
+		HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
 		break;
 	case 5:
-		HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
+		HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
 		break;
 	case 6:
-		HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
+		HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
 		break;
 	case 7:
-		HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, RESET);
+		HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
 		break;
 	case 8:
-		HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
+		HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
 		break;
 	case 9:
-		HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+		HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
 		break;
 	case 10:
-		HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, RESET);
+		HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
 		break;
 	case 11:
-		HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, RESET);
+		HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
 		break;
 	default:
 		break;
